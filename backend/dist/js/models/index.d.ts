86d06bb88c53793f4856954835a1286551461697
jsharples777 <jamie.sharples@gmail.com>
2021-08-13 12:14:50 +1000

    Server ts

--- backend/dist/js/models/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Account = require('./account');
-import BlogEntry = require('./blogentry');
-import Comment = require('./comment');
-export { Account, BlogEntry, Comment };
--- backend/dist/js/models/user.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize';
-interface UserAttributes {
-    id: number;
-    username: string;
-    password: string;
-}
-declare class User extends Model<UserAttributes> {
-}
-export = User;
